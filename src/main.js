03a07b0a85e901344acf391503a92420ffea54f7
lyy130 <1398857499@qq.com>
2019-09-15 23:38:40 +0800

    tabar组建

--- src/main.js
+++ src/main.js
@@ -6,16 +6,16 @@ import router from './router'
 import { Header } from 'mint-ui';
 Vue.component(Header.name, Header);
 import 'mint-ui/lib/style.css' //注意，此处样式要单独引入。
-import mui from './assets/dist/js/mui.min.js'
+// import mui from './assets/dist/js/mui.min.js'
 import './assets/dist/css/mui.min.css' //注意，此处样式要单独引入。
 import './assets/dist/css/icons-extra.css' //注意，此处样式要单独引入。
-Vue.prototype.mui = mui
+// Vue.prototype.mui = mui
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  router,                          /* 挂载路由在实例上*/
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,43 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import Router from 'vue-router'        /*引入路由包*/
 import HelloWorld from '@/components/HelloWorld'
+import home from '@/components/tabar/Home'
+import member from '@/components/tabar/Menber'
+import shopcar from '@/components/tabar/Shopcar'
+import search from '@/components/tabar/Search'
 
-Vue.use(Router)
+// import home from '@/components/tabar/Home.vue'   /* 导入组件包*/
 
-export default new Router({
+
+Vue.use(Router)        /*安装路由*/
+
+export default new Router({                     /* 创建路由对象，并将其暴露出去*/
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/home',                                          /* 这个是router-link对应的to值*/
+      name: 'home',
+      component: home                                       /* 这个是import后面的组建名*/
+    },
+    {
+      path: '/member',
+      name: 'member',
+      component: member
+    },
+    {
+      path: '/shopcar',
+      name: 'shopcar',
+      component: shopcar
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: search
     }
-  ]
+  ],
+  linkActiveClass:'mui-active'  //覆盖默认的路由高亮的类
 })
